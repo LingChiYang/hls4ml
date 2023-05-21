cde3a1526d223ad4a2cb650fd9a3ab9ce2f02fe2
YanLun-Huang <94730012+YanLunHuang@users.noreply.github.com>
2023-05-21 13:41:30 +0800

    Add array of stream version

--- hls4ml/templates/vivado/nnet_utils/nnet_sepconv1d_stream.h
+++ hls4ml/templates/vivado/nnet_utils/nnet_sepconv1d_stream.h
@@ -80,6 +80,42 @@ ReadInputWidth:
     }
 }
 
+template<class data_T, class res_T, typename CONFIG_T>
+void pointwise_conv_1d_cl_array(
+    hls::stream<data_T> data[CONFIG_T::n_chan],
+    hls::stream<res_T>  res[CONFIG_T::n_filt],
+    typename CONFIG_T::weight_t weights[CONFIG_T::n_chan * CONFIG_T::n_filt],
+    typename CONFIG_T::bias_t   biases[CONFIG_T::n_filt])
+{
+    assert(CONFIG_T::pad_left == 0 && CONFIG_T::pad_right == 0);
+    assert(CONFIG_T::filt_width == 1);
+
+    #pragma HLS ARRAY_PARTITION variable=weights complete
+    #pragma HLS ARRAY_PARTITION variable=biases complete
+
+    data_T data_store[CONFIG_T::n_chan];
+    #pragma HLS ARRAY_PARTITION variable=data_store complete
+    
+    ReadInputWidth: for (unsigned i_iw = 0; i_iw < CONFIG_T::in_width; i_iw++) {
+        if (CONFIG_T::strategy == nnet::latency) {
+            #pragma HLS PIPELINE II=CONFIG_T::reuse_factor
+        }
+        if (i_iw % CONFIG_T::stride_width == 0) {
+            
+            for(int j = 0; j < CONFIG_T::n_chan; j++){
+                #pragma HLS UNROLL
+                data_store[j] = data[j].read();
+            }
+            pointwise_mult_buffer_array<data_T, res_T, CONFIG_T>(data_store, res, weights, biases);
+        } else {
+            for(int j = 0; j < CONFIG_T::n_chan; j++){
+                #pragma HLS UNROLL
+                data_store[j] = data[j].read();
+            }
+        }
+    }
+}  
+  
 template <class data_T, class res_T, typename CONFIG_T>
 void separable_conv_1d_cl(hls::stream<data_T> &data, hls::stream<res_T> &res,
                           typename CONFIG_T::depthwise_config::weight_t
